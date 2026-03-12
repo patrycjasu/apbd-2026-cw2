75f01d18e95086e00d0a94219d6a11e847c777fb
Patrycja Suchecka <pati.suchecka@gmail.com>
2026-03-12 21:16:33 +0100

    Change feature 4

--- Zadanie/Zadanie/Program.cs
+++ Zadanie/Zadanie/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Feature 1");
 Console.WriteLine("Feature 2");
 Console.WriteLine("Feature 3");
-Console.WriteLine("Feature 4");
+Console.WriteLine("Crazy feature 4");
 
 static double CalculateAverage(int[] values)
 {
