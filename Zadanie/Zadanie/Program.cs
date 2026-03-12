606e0438f3d19a7845c23564ac751f9dac2495a5
Patrycja Suchecka <pati.suchecka@gmail.com>
2026-03-12 19:53:32 +0100

    Create new method

--- Zadanie/Zadanie/Program.cs
+++ Zadanie/Zadanie/Program.cs
@@ -3,3 +3,8 @@ Console.WriteLine("Feature 1");
 Console.WriteLine("Feature 2");
 Console.WriteLine("Feature 3");
 Console.WriteLine("Feature 4");
+
+static double CalculateAverage(int[] values)
+{
+    
+}
